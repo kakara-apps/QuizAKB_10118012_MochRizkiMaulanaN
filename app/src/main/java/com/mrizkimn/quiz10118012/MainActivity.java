a98e76ce43c83a07f89b06803355f58d3928a31b
kakara-apps <kakaraappsrizki@gmail.com>
2021-05-29 12:22:47 +0700

    Initial commit Quiz

---
+++ app/src/main/java/com/mrizkimn/quiz10118012/MainActivity.java
@@ -0,0 +1,19 @@
+package com.mrizkimn.quiz10118012;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//Moch Rizki Maulana N
+//10118012
+//IF-1
+//29-05-2021
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
